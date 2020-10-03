1c29b4e0c385c8db9a9e4b23d996f15e439aa33b
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-10-03 12:11:13 -0700

    set fuction for get address

--- project1/directions.py
+++ project1/directions.py
@@ -1,22 +1,26 @@
 import requests
 
+def get_name(lat, long, st, key):
+    origen = "http://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/Rest/?lat=" + str(lat) + "&lon=" + str(long) + "&state=" + str(state) + "&apikey=" + key + "&format=json&notStore=false&version=4.10"
+    answer = ""
+    response = requests.get(origen).json()
+    result = response['StreetAddresses'][0]['StreetAddress']#only gets the name of the avenue or street
+
+    # remove the addres number, leaving only the street or avenue name.
+    signal = 0
+    for i in result:
+        if i == ' ':#exeption if the name of the street has numbers i.e 19th avenue, Eugene
+            signal = 1
+        if i not in "0123456789" or signal == 1:#deletes the first numbers house. i.e. 1790 Alder St. is converted to Alder St.
+            answer += i
+
+    return answer[1:]
+
 #here is where we enter the coordinates and state
-lat = 44.038966
-long =-123.087558
+lat =   44.048882
+long = -123.093602
 state = "or"
 #my personal key I got when I registered to the website.
 key = "bf20a5bb85774b0c9e9b7b319c92040f"
 
-origen = "http://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/Rest/?lat=" + str(lat) + "&lon=" + str(long) + "&state=" + str(state) + "&apikey=" + key + "&format=json&notStore=false&version=4.10"
-
-answer = ""
-response = requests.get(origen).json()
-address = response['StreetAddresses'][0]['StreetAddress']
-
-#remove the addres number, although if the street has a number it will delete it too. Needs improvement.
-# for i in address:
-#     if i not in "0123456789":
-#         answer += i
-
-
-print(address)
+print(get_name(lat, long, state, key))
